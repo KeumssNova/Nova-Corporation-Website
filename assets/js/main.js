10a14be932b203011cfe83ce746f9879954eb3cf
salym da costa <salymdc@gmail.com>
2025-06-22 21:01:27 +0200

    finir la page service, le reste est fait en majorité

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,3 +167,18 @@ Promise.all([
 // }
 
 // animate();
+
+
+
+// carousel services
+let swiper = new Swiper(".default-carousel", {
+loop: true,
+pagination: {
+el: ".swiper-pagination",
+clickable: true,
+},
+navigation: {
+nextEl: ".swiper-button-next",
+prevEl: ".swiper-button-prev",
+},
+});
